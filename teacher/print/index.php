1cff218ebe94acc95188fce2f3277cdb929e1c13
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-11-18 20:27:53 +0700

    Hidden download button when printing

--- teacher/print/index.php
+++ teacher/print/index.php
@@ -12,10 +12,23 @@
   <title>Print PDF</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  <script type="text/javascript">
+    function printPage() {
+        //Get the print button and put it into a variable
+        var printButton = document.getElementById("printPageButton");
+        //Set the print button visibility to 'hidden' 
+        printButton.style.visibility = 'hidden';
+        //Print the page content
+        window.print()
+        //Set the print button to 'visible' again 
+        //[Delete this line if you want it to stay hidden after printing]
+        printButton.style.visibility = 'visible';
+    }
+</script>
 </head>
 <body> 
   <h1>Course: {Course name} - Quiz: {Quiz name}</h1>
-  <button onclick="window.print()">Download PDF</button>
+  <button id="printPageButton" onclick="printPage()">Download PDF</button>
   <div>
     <p><span>Question 1:</span> ADSDWASDZXCACWQ ?</p>
     <p class="correct">A. ADWDOZXIC</p>
--- teacher/question/index.php
+++ teacher/question/index.php
@@ -1,61 +1,15 @@
-<?php
-// Get question collections
-$questionCollection = $mydb->question;
-
-if (isset($_POST['btnAddQuestion'])) {
-  $insertQuestion = $questionCollection->insertOne([
-    'questionNumber' => '1',  // CLONE
-    'quizId' => $_SESSION['quizId'],
-    'courseId' => $_SESSION['courseId'],
-    'description' => htmlspecialchars($_POST['add-description']),
-    'option1' => htmlspecialchars($_POST['add-option1']),
-    'option2' => htmlspecialchars($_POST['add-option2']),
-    'option3' => htmlspecialchars($_POST['add-option3']),
-    'option4' => htmlspecialchars($_POST['add-option4']),
-    'level' => $_POST['add-lvlOption'],
-    'unitScore' => (int) $_POST['add-lvlOption'] * 10
-  ]);
-}
-
-if (isset($_POST['BtnDeleteQuestion'])) {
-  $deleteQuestion = $questionCollection->deleteOne([
-    '_id' => new MongoDB\BSON\ObjectId($_POST['questionId'])
-  ]);
-}
-
-if (isset($_POST['BtnEditQuestion'])) {
-  $updateQuestion = $questionCollection->updateOne(
-    [
-      '_id' => new MongoDB\BSON\ObjectId($_POST['questionId'])
-    ],
-    [
-      '$set' => [
-        'description' => htmlspecialchars($_POST['edit-description']),
-        'option1' => htmlspecialchars($_POST['edit-option1']),
-        'option2' => htmlspecialchars($_POST['edit-option2']),
-        'option3' => htmlspecialchars($_POST['edit-option3']),
-        'option4' => htmlspecialchars($_POST['edit-option4']),
-        'level' => $_POST['edit-lvlOption'],
-        'unitScore' => (int) $_POST['edit-lvlOption'] * 10,
-      ]
-    ]
-  );
-}
-?>
-
-<!-- Form Add -->
 <form class="form-question" action="./?page=question" method="POST" id="form-question">
   <div class="form-header">
     <div class="form-question-desc">
       <p>Question:<span class="wrong"></span></p>
-      <textarea name="add-description" rows="2" placeholder="Question Description"></textarea>
+      <textarea name="question" rows="2" placeholder="Question Description"  required></textarea>
     </div>
     <div class="form-level">
       <div class="form-select">
-        <select name="add-lvlOption">
-          <option value="1">Easy</option>
-          <option value="2">Medium</option>
-          <option value="3">Hard</option>
+        <select>
+          <option>Easy</option>
+          <option>Medium</option>
+          <option>Hard</option>
         </select>
       </div>
     </div>
@@ -63,41 +17,40 @@ if (isset($_POST['BtnEditQuestion'])) {
   <div class="form-body">
     <div class="form-option">
       <p>Option A<span style="color: red">*</span><span class="wrong"></span></p>
-      <textarea name="add-option1" rows="2" placeholder="Option A (correct answer)"></textarea>
+      <textarea name="A" rows="2" placeholder="Option A (correct answer)" required></textarea>
     </div>
     <div class="form-option">
       <p>Option B <span class="wrong"></span></p>
-      <textarea name="add-option2" rows="2" placeholder="Option B"></textarea>
+      <textarea name="B" rows="2" placeholder="Option B" required></textarea>
     </div>
     <div class="form-option">
       <p>Option C <span class="wrong"></span></p>
-      <textarea name="add-option3" rows="2" placeholder="Option C"></textarea>
+      <textarea name="C" rows="2" placeholder="Option C" required></textarea>
     </div>
     <div class="form-option">
       <p>Option D <span class="wrong"></span></p>
-      <textarea name="add-option4" rows="2" placeholder="Option D"></textarea>
+      <textarea name="D" rows="2" placeholder="Option D" required></textarea>
     </div>
   </div>
   <div class="form-button">
     <div class="cancel">Cancel</div>
-    <button class="submit" type="submit" name="btnAddQuestion">Submit</button>
+    <button class="submit" type="submit">Submit</button>
   </div>
 </form>
 
-<!-- Form Edit -->
 <div class="form-wrapper">
   <form class="form-edit" action="./?page=question" method="POST" id="form-edit">
     <div class="form-header">
       <div class="form-question-desc">
         <p>Question:<span class="wrong"></span></p>
-        <textarea name="edit-description" rows="2" placeholder="Question Description"></textarea>
+        <textarea name="question" rows="2" placeholder="Question Description" required></textarea>
       </div>
       <div class="form-level">
         <div class="form-select">
-          <select name="edit-lvlOption">
-            <option value="1">Easy</option>
-            <option value="2">Medium</option>
-            <option value="3">Hard</option>
+          <select>
+            <option value="0">Easy</option>
+            <option value="1">Medium</option>
+            <option value="2">Hard</option>
           </select>
         </div>
       </div>
@@ -105,38 +58,38 @@ if (isset($_POST['BtnEditQuestion'])) {
     <div class="form-body">
       <div class="form-option">
         <p>Option A<span style="color: red">*</span><span class="wrong"></span></p>
-        <textarea name="edit-option1" rows="2" placeholder="Option A (correct answer)"></textarea>
+        <textarea name="A" rows="2" placeholder="Option A (correct answer)"  required></textarea>
       </div>
       <div class="form-option">
         <p>Option B <span class="wrong"></span></p>
-        <textarea name="edit-option2" rows="2" placeholder="Option B"></textarea>
+        <textarea name="B" rows="2" placeholder="Option B" required></textarea>
       </div>
       <div class="form-option">
         <p>Option C <span class="wrong"></span></p>
-        <textarea name="edit-option3" rows="2" placeholder="Option C"></textarea>
+        <textarea name="C" rows="2" placeholder="Option C" required></textarea>
       </div>
       <div class="form-option">
         <p>Option D <span class="wrong"></span></p>
-        <textarea name="edit-option4" rows="2" placeholder="Option D"></textarea>
+        <textarea name="D" rows="2" placeholder="Option D" required></textarea>
       </div>
-      <!-- ==================================================== -->
-      <input type="hidden" name="questionId">
+      <!-- Input for questionID -->
+      <input type="hidden" name="questionID" value="1">
     </div>
     <div class="form-button">
-      <button type="submit" class="delete" name="BtnDeleteQuestion">Delete</button>
+      <button type="submit" class="delete">Delete</button>
       <div class="cancel">Cancel</div>
-      <button type="submit" class="submit" name="BtnEditQuestion">Submit</button>
+      <button class="submit" type="submit">Submit</button>
     </div>
   </form>
 </div>
 
 <div class="function-button">
-  <a href="./?page=result">
+  <a href="./?page=result&questionID=questionID">
     <button class="view">View results</button>
   </a>
-  <a href="./print">
-    <button class="download">Download PDF</button>
-  </a>
+  <!-- <button class="download">Download </button> -->
+  <button class="download"><a href ="./question/pdf.php">Download PDF</a></button>
+  <!-- <?php echo "<button class=\"download\" onClick=\"window.print();\">Download PDF</button>";?> -->
 </div>
 
 <?php
@@ -144,40 +97,33 @@ if (isset($_POST['BtnEditQuestion'])) {
 $questionCollection = $mydb->question;
 
 // Get question based on quizID and courseID
-$questions = $questionCollection->find(['quizId' => $_SESSION["quizId"], 'courseId' => $_SESSION["courseId"]]);
+$questions = $questionCollection->find(['quizID' => $_SESSION["quizID"], 'courseID' => $_SESSION["courseID"]]);
 
-$numberOfQuestion = 0;
 foreach ($questions as $question) {
-  $numberOfQuestion++;
   echo '<div class="card">
         <div class="card-top">
-          <p><span>Question ' . $numberOfQuestion . ': </span>' . $question->description . '</p>
+          <p><span>Question ' . $question->questionNumber . ': </span>' . $question->description . '</p>
           <i class="fa fa-edit">
             <input type="hidden" value="' . $question->description . '" />
-            <input type="hidden" value="' . $question->option1 . '">
-            <input type="hidden" value="' . $question->option2 . '">
-            <input type="hidden" value="' . $question->option3 . '">
-            <input type="hidden" value="' . $question->option4 . '">
+            <input type="hidden" value="' . $question->firstChoice . '">
+            <input type="hidden" value="' . $question->secondChoice . '">
+            <input type="hidden" value="' . $question->thirdChoice . '">
+            <input type="hidden" value="' . $question->fourthChoice . '">
             <!-- This is the level -->
             <input type="hidden" value="2" >
-            
-            <!-- This is the QuestionId -->
-            <input type="hidden" name="questionId" value="' . $question->_id . '"> 
+
+            <!-- This is the Quis ID -->
+            <input type="hidden" value="' . $question->questionNumber . '">
           </i>
         </div>
         <div class="answer">
-          <p class="correct">A: <span>' . $question->option1 . '</span></p>
-          <p>B: <span>' . $question->option2 . '</span></p>
-          <p>C: <span>' . $question->option3 . '</span></p>
-          <p>D: <span>' . $question->option4 . '</span></p>
+          <p class="correct">A: <span>' . $question->firstChoice . '</span></p>
+          <p>B: <span>' . $question->secondChoice . '</span></p>
+          <p>C: <span>' . $question->thirdChoice . '</span></p>
+          <p>D: <span>' . $question->fourthChoice . '</span></p>
         </div>
       </div>';
 }
 ?>
 
 <script src="./question/question.js"></script>
-<script>
-  window.onload = function() {
-    history.replaceState("", "", "./?page=question");
-  }
-</script>
