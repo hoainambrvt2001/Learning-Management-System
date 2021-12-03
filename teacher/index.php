3e647f1c20b1a8f8cdc431e26364816fecbb6841
hoainambrvt2001 <hoainambrvt2001@gmail.com>
2021-12-03 09:07:03 +0700

    quiz-maker-app

--- teacher/index.php
+++ teacher/index.php
@@ -70,7 +70,7 @@ if (!isset($_SESSION['username']) && $_SESSION['username'] == NULL) {
   ?>
   <link rel="stylesheet" href="./teacher.css">
   <link rel="stylesheet" href="./<?php echo "$page/$page"; ?>.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
